e9dae6e5599ce79ab2c7f0f9682b903176b88d89
ROBERT-Gimenez <robertgim37@gmail.com>
2025-05-11 17:40:18 -0300

    feat: add AuthMiddleware to handle JWT authentication

---
+++ middlewares/auth_middleware.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "No autorizado", http.StatusUnauthorized)
+			return
+		}
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		claims := &jwt.StandardClaims{}
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return []byte("secret_key"), nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Token inválido", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
